79fbb723ff566cd6917dd8930c4432b01c5313e6
Administrator <Administrator@172.31.2.42>
2017-10-17 11:18:36 +0000

    commit

---
+++ WebsterPOC/src/main/docker/wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while ! exec 6<>/dev/tcp/${DB_HOST}/${DB_PORT}; do
+    echo "Trying to connect to MySQL at ${DB_PORT}..."
+    sleep 10
+done
+
+java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /app.jar
